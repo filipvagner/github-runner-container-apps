68d3fe91c6d0ae9a3f24688b237f8b538cde73ae
Filip Vagner <filip.vagner@orbit.cz>
2025-07-31 23:19:27 +0200

    Added configuration files

---
+++ main.tf
@@ -0,0 +1,70 @@
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = var.log_analytics_workspace_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  sku                 = var.log_analytics_workspace_sku
+  retention_in_days   = var.log_analytics_workspace_retention_in_days
+  tags                = var.tags
+}
+
+resource "azurerm_container_app_environment" "this" {
+  name                               = var.container_app_environment_name
+  location                           = var.location
+  resource_group_name                = azurerm_resource_group.this.name
+  infrastructure_resource_group_name = var.infrastructure_resource_group_enabled ? "${var.resource_group_name}-managed" : null
+  infrastructure_subnet_id           = var.container_app_environment_subnet_id
+  internal_load_balancer_enabled     = var.container_app_environment_internal_load_balancer_enabled
+  zone_redundancy_enabled            = var.container_app_environment_zone_redundancy_enabled
+  log_analytics_workspace_id         = azurerm_log_analytics_workspace.this.id
+  logs_destination                   = "log-analytics"
+  mutual_tls_enabled                 = false
+  tags                               = var.tags
+
+  workload_profile {
+    name                  = "Consumption"
+    workload_profile_type = "Consumption"
+    minimum_count         = 0
+    maximum_count         = 0
+  }
+}
+
+resource "azurerm_container_app" "this" {
+  for_each = var.container_app
+
+  container_app_environment_id = azurerm_container_app_environment.this.id
+  name                         = each.value.name
+  resource_group_name          = azurerm_resource_group.this.name
+  revision_mode                = "Single"
+  tags                         = var.tags
+
+  template {
+    min_replicas = each.value.min_replicas
+    max_replicas = each.value.max_replicas
+
+    container {
+      name   = each.value.container.name
+      image  = each.value.container.image
+      cpu    = each.value.container.cpu
+      memory = each.value.container.memory
+    }
+
+    dynamic "custom_scale_rule" {
+      for_each = try(each.value.custom_scale_rule, null) != null ? [each.value.custom_scale_rule] : []
+      content {
+        name             = custom_scale_rule.value.name
+        custom_rule_type = custom_scale_rule.value.custom_rule_type
+        metadata         = custom_scale_rule.value.metadata
+      }
+    }
+  }
+
+  #TODO - Add more configurations as needed for the Container App
+  #secret block
+
+}
---
+++ variables.tf
@@ -0,0 +1,84 @@
+variable "location" {
+  description = "The Azure region where resources will be deployed."
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "log_analytics_workspace_name" {
+  description = "The name of the Log Analytics workspace."
+  type        = string
+}
+
+variable "log_analytics_workspace_sku" {
+  description = "The SKU of the Log Analytics workspace."
+  type        = string
+  default     = "PerGB2018"
+}
+
+variable "log_analytics_workspace_retention_in_days" {
+  description = "The retention period in days for the Log Analytics workspace."
+  type        = number
+  default     = 30
+}
+
+variable "container_app_environment_name" {
+  description = "The name of the Container App Environment."
+  type        = string
+}
+
+variable "infrastructure_resource_group_enabled" {
+  description = "Place resources in Azure managed resource group (false) or in the same resource group as the Container App Environment (true)."
+  type        = bool
+  default     = false
+}
+
+variable "container_app_environment_subnet_id" {
+  description = "The subnet ID for the Container App Environment."
+  type        = string
+}
+
+variable "container_app_environment_internal_load_balancer_enabled" {
+  description = "Enable internal load balancer for the Container App Environment."
+  type        = bool
+  default     = true
+}
+
+variable "container_app_environment_zone_redundancy_enabled" {
+  description = "Enable zone redundancy for the Container App Environment."
+  type        = bool
+  default     = false
+}
+
+variable "container_app" {
+  description = "List of container app template configurations."
+  type = map(object({
+    name         = string
+    min_replicas = optional(number, 0)
+    max_replicas = optional(number, 2)
+    container = optional(object({
+      name   = string
+      image  = string
+      cpu    = number
+      memory = string
+      #env block (name, secret_name, value)
+      args    = optional(list(string))
+      command = optional(list(string))
+    }))
+    custom_scale_rule = optional(object({
+      name             = string
+      custom_rule_type = string
+      metadata         = map(string)
+    }))
+  }))
+  default = {}
+}
